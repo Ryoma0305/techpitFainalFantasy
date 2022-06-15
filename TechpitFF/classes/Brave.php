b54f9936229b474ab65e9d47b4eaa42c73762ae1
EC2 Default User <ec2-user@ip-172-31-0-35.us-west-1.compute.internal>
2022-06-15 08:06:56 +0000

    add jobs

--- TechpitFF/classes/Brave.php
+++ TechpitFF/classes/Brave.php
@@ -4,17 +4,22 @@ class Brave extends Human
 {
     const MAX_HITPOINT = 120;
     public $hitPoint = self::MAX_HITPOINT;
-    public $attackPoint = 30;
+    private $attackPoint = 30;
+    
+    public function __construct($name)
+    {
+        parent::__construct($name, $this->hitPoint, $this->attackPoint);
+    }
     
     public function doAttack($enemy)
     {
         // 1/3 の確率でスキルを発動
         if(rand(1,3) === 1){
             //スキルの発動
-            echo "「" . $this->name . "」のスキルが発動した！\n";
+            echo "「" . $this->getName() . "」のスキルが発動した！\n";
             echo "「せんりょくぎり」!!\n";
-            echo $enemy->name . "に" . $this->attackPoint * 1.5 . "のダメージ!\n";
-            $enemy->tookDamage($this->attackPoint * 1.5);
+            echo $enemy->getName() . "に" . $this->getAttackPoint() * 1.5 . "のダメージ!\n";
+            $enemy->tookDamage($this->getAttackPoint() * 1.5);
         }else{
             parent::doAttack($enemy);
         }
